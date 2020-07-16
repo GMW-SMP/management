ef28f152f48148df935d0a2f7c6f0cece6e743e2
flogic <gamerking195@gmail.com>
2020-07-15 21:09:47 -0600

    Fix start script.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-PAPER_JAR=$(yq r config.yaml server-directory)/paper.jar
+SERVER_DIR=$(yq r config.yaml server-directory)
 TMUX_SESSION=$(yq r config.yaml tmux-session-name)
 
-tmux new-session -s $TMUX_SESSION java -Xms7680M -Xmx7680M -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar $PAPER_JAR --world-container worlds
+cd $SERVER_DIR
+
+tmux new-session -s $TMUX_SESSION java -Xms7680M -Xmx7680M -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar $SERVER_DIR/paper.jar --world-container worlds
