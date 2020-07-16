7451618c39eea95c929831ea752ed4dd34dde379
flogic <gamerking195@gmail.com>
2020-07-15 19:39:05 -0600

    Remove version from paper files.

--- scripts/update-paper.sh
+++ scripts/update-paper.sh
@@ -7,7 +7,7 @@ SERVER_DIR="$(yq r config.yaml server-directory)"
 echo "Ensuring PaperMC installation is V$MC_VERSION #$PAPER_BUILD"
 if [[ ! -f version_history.json || ! -f paper ]]; then
     echo "Missing files detected, downloading latest Paper build for Minecraft $MC_VERSION."
-    wget https://papermc.io/api/v1/paper/$MC_VERSION/$PAPER_BUILD/download -O $SERVER_DIR/paper-$MC_VERSION.jar
+    wget https://papermc.io/api/v1/paper/$MC_VERSION/$PAPER_BUILD/download -O $SERVER_DIR/paper.jar
   else
     CURRENT_BUILD=`cat version_history.json | jq '.currentVersion' | cut -d'-' -f 3 | cut -d' ' -f 1`
     LATEST_BUILD=`curl -s https://papermc.io/api/v1/paper/$MC_VERSION/latest | jq '.build' | cut -d'"' -f 2`
@@ -16,13 +16,13 @@ if [[ ! -f version_history.json || ! -f paper ]]; then
       echo "Current Paper $MC_VERSION build is #$CURRENT_BUILD. Latest build is #$LATEST_BUILD."
       if [[ $CURRENT_BUILD != $LATEST_BUILD]]; then
         echo "Updating Paper to build #$LATEST_BUILD."
-        wget https://papermc.io/api/v1/paper/$MC_VERSION/latest/download -O $SERVER_DIR/paper-$MC_VERSION.jar
+        wget https://papermc.io/api/v1/paper/$MC_VERSION/latest/download -O $SERVER_DIR/paper.jar
       fi
     else
       echo "Current Paper $MC_VERSION build is set to #$CURRENT_BUILD. Server build set to #$PAPER_BUILD."
       if [[ $CURRENT_BUILD != $PAPER_BUILD]]; then
         echo "Updating Paper to build #$PAPER_BUILD."
-        wget https://papermc.io/api/v1/paper/$MC_VERSION/$PAPER_BUILD/download -O $SERVER_DIR/paper-$MC_VERSION.jar
+        wget https://papermc.io/api/v1/paper/$MC_VERSION/$PAPER_BUILD/download -O $SERVER_DIR/paper.jar
       fi
     fi
   fi
