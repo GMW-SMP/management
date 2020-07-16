4b4a972250cd1a896cd0e45c591e7b6b71fd2c76
flogic <gamerking195@gmail.com>
2020-07-15 18:17:17 -0600

    Fix paper update script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,16 +2,16 @@
 
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 FILENAME=gmw-backup-$DATE.tar.gz
-PROJ_BASE_DIR=$1
-BACKUP_TYPE=$2
-RETENTION_DAYS=$3
+PROJ_BASE_DIR=$(yq r config.yaml server-directory)
+BACKUP_TYPE=$1
+RETENTION_DAYS=$2
 
 echo "Creating backup directory."
 mkdir -p $PROJ_BASE_DIR/backups/$BACKUP_TYPE
 
 echo "Creating server archive."
-cd $PROJ_BASE_DIR/server/
-tar -czvf $PROJ_BASE_DIR/backups/$BACKUP_TYPE/$FILENAME logs plugins worlds bukkit.yml bukkit.yml paper.yml spigot.yml whitelist.json server.properties banned-ips.json banned-players.json
+cd $PROJ_BASE_DIR/
+tar -czvf $PROJ_BASE_DIR/../backups/$BACKUP_TYPE/$FILENAME logs worlds whitelist.json banned-ips.json banned-players.json
 
 echo "Purging backups over 30 days old."
 find $BACKUP_DIR -type f -name 'gmw-backup-*.tar.gz' -mtime +$RETENTION_DAYS -print -exec rm {} \; >> $LOG_FILE
--- scripts/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-MC_DIR = $1
-
-if test -f "$MC_DIR"; then
-  rm -r $MC_DIR/plugins
-fi
