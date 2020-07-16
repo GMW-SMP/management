dcea8f1c396e3a3ad8edab8e8eb0898dcfd11753
flogic <gamerking195@gmail.com>
2020-07-15 19:03:22 -0600

    Update start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 PAPER_JAR=$(yq r config.yaml server-directory)/paper.jar
+TMUX_SESSION=$(yq r config.yaml tmux-session-name)
 
-tmux new-session -s smp-server java -Xms7680M -Xmx7680M -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar $PAPER_JAR --world-container worlds
+tmux new-session -s $TMUX_SESSION java -Xms7680M -Xmx7680M -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar $PAPER_JAR --world-container worlds
