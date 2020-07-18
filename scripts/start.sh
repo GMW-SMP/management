c63795c9cb42e1c2920cf050796a54418a7b746e
flogic <gamerking195@gmail.com>
2020-07-17 21:42:09 -0600

    Make tmux session start in detached mode.

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,4 +5,4 @@ TMUX_SESSION=$(yq r config.yaml tmux-session-name)
 
 cd $SERVER_DIR
 
-tmux new-session -s $TMUX_SESSION java -Xms7680M -Xmx7680M -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar $SERVER_DIR/paper.jar --world-container worlds
+tmux new-session -ds $TMUX_SESSION java -Xms7680M -Xmx7680M -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar $SERVER_DIR/paper.jar --world-container worlds
